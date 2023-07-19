620710af05abf6838747d47bc18632bd4946de0f
Kasper Eloranta <kasper.eloranta@tuni.fi>
2023-07-19 12:49:51 +0300

    dry manure config

--- logistics_sim/waste_pickup_sim_test.py
+++ logistics_sim/waste_pickup_sim_test.py
@@ -5,11 +5,11 @@ import random
 
 sim_config = {	
 	'sim_name': 'Biomass transportation to biogas facility',
-	'sim_type': 1, # 1=Grass and straws, 2=dry manures, 3=slurry manures
-	'sim_runtime_days': 28, # Simulation runtime in days, (Kalenterivuoden 2022 työpäivien määrä) , ylläpidetään myös: routing_optimizer.cpp rivi 136 ! 
-	'pickup_sites_filename': 'geo_data/nearest_pickup_sites_nurmetoljet.geojson',
+	'sim_type': 2, # 1=Grass and straws, 2=dry manures, 3=slurry manures
+	'sim_runtime_days': 228, # Simulation runtime in days, (Kalenterivuoden 2022 työpäivien määrä) , ylläpidetään myös: routing_optimizer.cpp rivi 136 ! 
+	'pickup_sites_filename': 'geo_data/nearest_pickup_sites_kuivalannat.geojson',
 	'depots_filename': 'geo_data/sim_test_terminals.geojson', # Nyt depots = terminals, vain 1, Biokaasulaitos. 
-	'depot_capacity' : 28000, # yearly targeted input for facility
+	'depot_capacity' : 14000, # yearly targeted input for facility
 	'terminals_filename': 'geo_data/sim_test_terminals.geojson', 
 	'vehicle_template': {
 		'load_capacity': 45, # Keskim. arvaus
