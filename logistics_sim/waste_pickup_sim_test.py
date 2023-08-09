1c1cd928fbea3e8e803bf18ba9a22205c9d02f81
Kasper Eloranta <kasper.eloranta@tuni.fi>
2023-08-09 11:50:31 +0300

    finetuning

--- logistics_sim/waste_pickup_sim_test.py
+++ logistics_sim/waste_pickup_sim_test.py
@@ -8,7 +8,7 @@ sim_config = {
 	'sim_name': 'Biomass transportation to biogas facility',
 	'sim_type': 2, # 1=Grass and straws, 2=manures
 	'isTimeCriticalityConsidered' :  'False', # Boolean
-	'sim_runtime_days': 228, # Simulation runtime in days 
+	'sim_runtime_days': 228, # Simulation runtime in days, kalenterivuoden 2022 tyopaivat
 	'pickup_sites_filename': 'geo_data/nearest_pickup_sitesKuivalannat.geojson',
 	'depots_filename': 'geo_data/sim_test_terminals.geojson', # Nyt depots = terminals, vain 1, Biokaasulaitos. 
 	'depot_capacity' : 14000, # yearly targeted input for facility
@@ -18,15 +18,15 @@ sim_config = {
 		'max_route_duration': 8*60 + 15, # Minutes (9h - 45min break = 8h 15min) 
 		'break_duration': 45, # Minutes # Break Happens after 1/2 of drivetime 
 		'num_breaks_per_shift': 1,
-		'pickup_duration': 10, # Minutes # Tämä 10 min = keruiden asetusaika, sama kaikille biomassoille. Simulaatiossa ja optimoinnissa huomioidaan keruun keston lineaarinen komponentti.
-							  # Vakiokomponenttia ylläpidetään myös: routing_optimizer.cpp rivi 22
+		'pickup_duration': 10, # Minutes # Tama 10 min = keruiden asetusaika, sama kaikille biomassoille. Simulaatiossa ja optimoinnissa huomioidaan keruun keston lineaarinen komponentti.
+							  # Vakiokomponenttia yllapidetaan myos: routing_optimizer.cpp rivi 22
 							  # Lin. kommponentti simulaatiossa: waste_pickup_sim.py rivit 254 ja 266  
 							  #  Lin. kommponentti optimoijassa: LogisticsSimulation::pickup
 		'load_TS_rate': 0.0
 	},
 	'depots': [
 		{
-			'num_vehicles': 10 # TESTAA MYÖS ERI ARVOILLA
+			'num_vehicles': 10 # TESTAA MYOS ERI ARVOILLA
 		}
 	]
 }
