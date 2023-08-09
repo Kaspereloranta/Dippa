04166b753b6763b772121d13a9e8f80a6d83efaa
Kasper Eloranta <kasper.eloranta@tuni.fi>
2023-08-09 22:34:53 +0300

    adjust weight of productionstoppages cost term

--- logistics_sim/routing_optimizer.cpp
+++ logistics_sim/routing_optimizer.cpp
@@ -623,7 +623,7 @@ double costFunctionFromComponents(double totalOdometer, double totalNumPickupSit
   + totalNumPickupSiteOverloadDays*50.0 // Penalty of 50 eur / overload day / pickup site
   + totalOvertime*(50.0/60) // Cost of 50 eur / h for overtime work  
   + dilutionWater*1
-  + productionStoppages*500
+  + productionStoppages*1000000
   + overFillings*1000
   + unnecessaryImports*100;
 }
