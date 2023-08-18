c24a921499900c7da370e994066a71c17168496f
Kasper Eloranta <kasper.eloranta@tuni.fi>
2023-08-18 16:10:23 +0300

    add debug prints to c++ simulation

--- logistics_sim/routing_optimizer.cpp
+++ logistics_sim/routing_optimizer.cpp
@@ -905,7 +905,7 @@ int main() {
   
   // TÄÄLLÄ MÄÄRÄTÄÄN KUINKA MONTA KIERROSTA GEENIAJOJA TEHDÄÄN, VAIKUTTA OPTIMOINNIN NOPEUTEEN, VOIDAAN MYÖS LISÄTÄ GEENEJÄ JOS HALUTAAN TARKENTAA LASKENTAA
   
-  int numGenerations = 5 // 100000; // 40000
+  int numGenerations = 5; // 100000; // 40000
   int numFinetuneGenerations = 5; // 20000
   int numGenerationsPerStep = 100;
   //optimizer.initPopulation();
